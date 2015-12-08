fc6803bb43d1d8c898376bfede51968114cbb5f0
scenroul <584869305@qq.com>
2015-12-08 17:28:33 +0800

    login code

--- js/tools.js
+++ js/tools.js
@@ -1,1 +1,44 @@
 /* Javascript Document create by tw 2015-12-8*/
+function getHost()
+{
+	return "http://sa.cmcc.com/";
+}
+/**
+ * 以下本地缓存
+ */
+function getLocalCache(key) 
+{
+    var result;
+    var storage = window.localStorage;
+    if (storage) 
+	{
+        result = storage.getItem(key);
+    }
+    return result;
+}
+
+function setLocalCache(key, value) 
+{
+    var storage = window.localStorage;
+    if (storage) 
+	{
+        storage.removeItem(key);
+        storage.setItem(key, value);
+    }
+}
+function removeLocalCache(key) 
+{
+    var storage = window.localStorage;
+    if (storage) 
+	{
+        storage.removeItem(key);
+    }
+}
+function clearLocalCache()
+{
+	var storage = window.localStorage;
+    if (storage) 
+	{
+        storage.clear();
+    }
+}
