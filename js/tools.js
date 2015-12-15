0d57d224082353371aad609207c41579065620da
scenroul <584869305@qq.com>
2015-12-15 13:28:57 +0800

    checkall test

--- js/tools.js
+++ js/tools.js
@@ -323,6 +323,6 @@ function loadingText()
 }
 function checkAll()
 {
-	alert($("#line_check").attr("checked"));
-	$("input[class='line_check']").attr("checked",$("#line_check").attr("checked")==true);
+	alert($("input[id=='line_check']").attr("checked"));
+	$("input[class='line_check']").attr("checked",$("input[id=='line_check']").attr("checked")==true);
 }
